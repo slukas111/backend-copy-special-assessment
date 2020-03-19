719103d52d2aa1f6abc02faad5eebe23bda041c8
slukas111 <sashamati@gmail.com>
2020-03-19 15:50:29 -0700

    create a function to file path

--- copyspecial.py
+++ copyspecial.py
@@ -16,13 +16,17 @@ import subprocess
 import argparse
 
 # This is to help coaches and graders identify student assignments
-__author__ = "???"
+__author__ = "Sasha Lukas"
 
 
-# +++your code here+++
-# Write functions and modify main() to call them
-
-def main():
+def get_paths(dir):
+    """manipulate file paths"""
+    new_paths = []
+    files = os.listdir(dir)     
+    for file in files:
+         
+    
+    def main():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
